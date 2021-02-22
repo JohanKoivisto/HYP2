5b026aaa7608f6a70944e8edf9f102a14119cdce
JohanKoivisto <60185431+JohanKoivisto@users.noreply.github.com>
2021-02-22 10:45:51 +0200

    responsiveness, sass, a lot of rewrite

--- js/script.js
+++ js/script.js
@@ -1,105 +1,17 @@
-$(document).ready(function() {
-    
-    
-    /* For the sticky navigation */
-    $('.js--section-hyp').waypoint(function(direction) {
-        if (direction == "down") {
-            $('nav').addClass('sticky');
-        } else {
-            $('nav').removeClass('sticky');
-        }
-    }, {
-      offset: '60px;'
-    });
-    
-    
-    /* Scroll on buttons */
-    
-    /* Navigation scroll */
-    $(function() {
-      $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top
-            }, 1000);
-            return false;
-          }
-        }
-      });
-    });
-    
-    
-    /* Animations on scroll */
-    /* $('.js--wp-1').waypoint(function(direction) {
-        $('.js--wp-1').addClass('animated fadeIn');
-    }, {
-        offset: '50%'
-    });
-    
-    $('.js--wp-2').waypoint(function(direction) {
-        $('.js--wp-2').addClass('animated fadeInUp');
-    }, {
-        offset: '50%'
-    });
-    
-    $('.js--wp-3').waypoint(function(direction) {
-        $('.js--wp-3').addClass('animated fadeIn');
-    }, {
-        offset: '50%'
-    });
-    
-    $('.js--wp-4').waypoint(function(direction) {
-        $('.js--wp-4').addClass('animated pulse');
-    }, {
-        offset: '50%'
-    }); */
-    
-    
-    /* Mobile navigation */
-    $('.js--nav-icon, .js--main-nav a, .logo-black').click(function(element){
-        var nav = $('.js--main-nav');
-        var icon = $('.js--nav-icon i');
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
 
-        //Gets the class name of the element that triggered the event
-        var clicked = element.target.className;
+// Get the navbar
+var navbar = document.getElementById("navbar");
 
-        //Exists the function if the menu is closed AND the logo-black element (logo image) was clicked
-        if (icon.hasClass('ion-navicon-round') && clicked == 'logo-black')
-            return;
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
 
-        //Opens and closes the menu
-        if ($(window).width() < 768){
-            nav.slideToggle(200);
-        }
-
-        //Changes icon states of the menu button
-        if (icon.hasClass('ion-navicon-round')) {
-            icon.addClass('ion-close-round');
-            icon.removeClass('ion-navicon-round');
-        } else {
-            icon.addClass('ion-navicon-round');
-            icon.removeClass('ion-close-round');
-        }
-    });
-
-
-    $(window).resize(function(){
-        var nav = $('.js--main-nav');
-        var icon = $('.js--nav-icon i');
-
-        if ($(window).width() > 767){
-            nav.css("display", "block");
-            icon.addClass('ion-close-round');
-            icon.removeClass('ion-navicon-round');
-        } else {
-            nav.css("display", "none");
-            icon.addClass('ion-navicon-round');
-            icon.removeClass('ion-close-round');
-        }
-
-    });
-});
-  
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+} 
